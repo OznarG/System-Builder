4c20412582c8c4532414049b64684fabf921c5d2
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2025-09-04 05:42:28 -0400

    Added camera for FP

--- SystemBuilder/Assets/_Player/PlayerController.cs
+++ SystemBuilder/Assets/_Player/PlayerController.cs
@@ -3,13 +3,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float jumHeight = 2f;
-    [SerializeField] private float gravity = -9.8f;
 
     private CharacterController controller;
-    private Vector3 moveInput;
-    private Vector3 velocity;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,23 +13,9 @@ public class PlayerController : MonoBehaviour
         controller = GetComponent<CharacterController>();
     }
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-        Debug.Log($"Move Input: {moveInput}");
-    }
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        Debug.Log($"Jumping: {context.performed} - Is Grounded: {controller.isGrounded}");
-        if(context.performed && controller.isGrounded)
-        {
-            Debug.Log("We are Supposed to jump");
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }

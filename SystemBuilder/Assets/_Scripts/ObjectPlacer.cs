a93ba4be4da7d9ed40cce0cbaa099f07c777e74d
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2025-09-17 03:25:08 -0400

    Added placement preview functionality

--- SystemBuilder/Assets/_Scripts/ObjectPlacer.cs
+++ SystemBuilder/Assets/_Scripts/ObjectPlacer.cs
@@ -3,17 +3,18 @@ using UnityEngine.InputSystem;
 
 public class ObjectPlacer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject placeableObjectPrefab;
+    [SerializeField] private GameObject previewObjectPrefab;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private LayerMask placementSurfaceLayerMask;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private float objectDistantceFromPlayer;
+    [SerializeField] private float raycastStartVerticalOffset;
+    [SerializeField] private float raycastDistance;
+
+    private GameObject _previewObject = null;
+    private Vector3 _currentPlacementPosition = Vector3.zero;
+    private bool _inplacementMode = false;
 
     void OnTESTKEY(InputValue value)
     {
@@ -30,13 +31,44 @@ public class ObjectPlacer : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if(_inplacementMode)
+        {
+            UpdateCurrentPlacementPosition();
+        }
+    }
+    private void UpdateCurrentPlacementPosition()
+    {
+        Vector3 cameraForward = new Vector3(playerCamera.transform.forward.x, 0F, playerCamera.transform.forward.z);
+        cameraForward.Normalize();
+
+        Vector3 startPos = playerCamera.transform.position + (cameraForward * objectDistantceFromPlayer);
+        startPos.y += raycastStartVerticalOffset;
+
+        RaycastHit hitinfo;
+        if(Physics.Raycast(startPos, Vector3.down, out hitinfo, raycastDistance, placementSurfaceLayerMask)) 
+        {
+            _currentPlacementPosition = hitinfo.point;
+        }
+
+        Quaternion rotation = Quaternion.Euler(0f, playerCamera.transform.eulerAngles.y, 0f);
+        _previewObject.transform.position = _currentPlacementPosition;
+        _previewObject.transform.rotation = rotation;
+        Debug.DrawRay(startPos, Vector3.down * raycastDistance, Color.red);
+    }
     private void EnterPlacementMode()
     {
         Debug.Log("Entering Placement Mode");
+
+        Quaternion rotation = Quaternion.Euler(0f, playerCamera.transform.eulerAngles.y, 0f);
+        _previewObject = Instantiate(previewObjectPrefab, _currentPlacementPosition, rotation);
+        _inplacementMode = true;    
     }
     
     private void ExitPlacementMode()
     {
         Debug.Log("Exiting Placement Mode");
+        _inplacementMode = false;
     }
 }

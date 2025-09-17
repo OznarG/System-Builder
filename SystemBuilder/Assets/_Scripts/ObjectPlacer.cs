36de326b8e160c9fe346aee1edb036bd5951883d
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2025-09-17 05:40:00 -0400

    Added color to know if is valid or not to place object

--- SystemBuilder/Assets/_Scripts/ObjectPlacer.cs
+++ SystemBuilder/Assets/_Scripts/ObjectPlacer.cs
@@ -3,11 +3,18 @@ using UnityEngine.InputSystem;
 
 public class ObjectPlacer : MonoBehaviour
 {
+    [Header("Placement Parameters")]
     [SerializeField] private GameObject placeableObjectPrefab;
     [SerializeField] private GameObject previewObjectPrefab;
     [SerializeField] private Camera playerCamera;
     [SerializeField] private LayerMask placementSurfaceLayerMask;
 
+    [Header("Preview Materials")]
+    [SerializeField] private Material previewMaterial;
+    [SerializeField] private Color valisColor;
+    [SerializeField] private Color invalidColor;
+
+    [Header("Raycast Parameters")]
     [SerializeField] private float objectDistantceFromPlayer;
     [SerializeField] private float raycastStartVerticalOffset;
     [SerializeField] private float raycastDistance;
@@ -15,7 +22,9 @@ public class ObjectPlacer : MonoBehaviour
     private GameObject _previewObject = null;
     private Vector3 _currentPlacementPosition = Vector3.zero;
     private bool _inplacementMode = false;
+    private bool _valistBuildState = false;
 
+    #region Inputs
     void OnTESTKEY(InputValue value)
     {
         if(value.isPressed)
@@ -30,30 +39,32 @@ public class ObjectPlacer : MonoBehaviour
             ExitPlacementMode();
         }
     }
-
     //This needs to be moved or replaced
     void OnInteract(InputValue value)
     {
         if (value.isPressed)
         {
-            PlceObject();
+            PlaceObject();
         }
     }
+    #endregion
 
     private void Update()
     {
         if(_inplacementMode)
         {
             UpdateCurrentPlacementPosition();
+
+            if(CanPlaceObject())
+            {
+                SetValidPreviewState();
+            }
+            else
+                SetInvalidPreviewState();
         }
     }
-    private void PlceObject()
-    {
-        Quaternion rotation = Quaternion.Euler(0f, playerCamera.transform.eulerAngles.y, 0f);
-        Instantiate(placeableObjectPrefab, _currentPlacementPosition, rotation, transform);
 
-        ExitPlacementMode();
-    }
+
     private void UpdateCurrentPlacementPosition()
     {
         Vector3 cameraForward = new Vector3(playerCamera.transform.forward.x, 0F, playerCamera.transform.forward.z);
@@ -73,9 +84,24 @@ public class ObjectPlacer : MonoBehaviour
         _previewObject.transform.rotation = rotation;
         Debug.DrawRay(startPos, Vector3.down * raycastDistance, Color.red);
     }
+    #region Placement Functions
+    private void PlaceObject()
+    {
+        if (!_inplacementMode || !_valistBuildState)
+        {
+            return;
+        }
+        Quaternion rotation = Quaternion.Euler(0f, playerCamera.transform.eulerAngles.y, 0f);
+        Instantiate(placeableObjectPrefab, _currentPlacementPosition, rotation, transform);
+
+        ExitPlacementMode();
+    }
     private void EnterPlacementMode()
     {
-        Debug.Log("Entering Placement Mode");
+        if (_inplacementMode)
+        {
+            return;
+        }
 
         Quaternion rotation = Quaternion.Euler(0f, playerCamera.transform.eulerAngles.y, 0f);
         _previewObject = Instantiate(previewObjectPrefab, _currentPlacementPosition, rotation, transform);
@@ -84,9 +110,26 @@ public class ObjectPlacer : MonoBehaviour
     
     private void ExitPlacementMode()
     {
-        Debug.Log("Exiting Placement Mode");
+
         _inplacementMode = false;
         Destroy(_previewObject);
         _previewObject= null;
     }
+    private bool CanPlaceObject()
+    {
+        if(_previewObject == null)
+            return false;
+        return _previewObject.GetComponentInChildren<PrevValidPlacement>().IsValid;
+    }
+    private void SetValidPreviewState()
+    {
+        previewMaterial.color = valisColor;
+        _valistBuildState = true;
+    }
+    private void SetInvalidPreviewState()
+    {
+        previewMaterial.color = invalidColor;
+        _valistBuildState = false;
+    }
+    #endregion
 }
---
+++ SystemBuilder/Assets/_Scripts/PrevValidPlacement.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrevValidPlacement : MonoBehaviour
+{
+    [SerializeField] private LayerMask invalidLayers;
+    public bool IsValid { get; private set; } = true;
+    private HashSet<Collider> _colldingObjects = new HashSet<Collider>();
+
+    // bitwise left shift opperator (<<) and bitwise and operator (&) ---> needs research
+    private void OnTriggerEnter(Collider other)
+    {
+        if(((1 << other.gameObject.layer) != 0))
+        {
+            _colldingObjects.Add(other);
+            IsValid = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(((1 << other.gameObject.layer) & invalidLayers) != 0)
+        {
+            _colldingObjects.Remove(other);
+            IsValid = _colldingObjects.Count <= 0;
+        }
+    }
+}

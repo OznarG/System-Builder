86b5bac9bc5ca01054918e1e134c493e3715e1ee
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2025-09-18 05:20:37 -0400

    Added Time

--- SystemBuilder/Assets/_Scripts/Managers/TimeManager.cs
+++ SystemBuilder/Assets/_Scripts/Managers/TimeManager.cs
@@ -1,16 +1,69 @@
+using System;
+using TMPro;
 using UnityEngine;
+
+#region Enums
+public enum DayEnum
+{
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
+
+public enum StationEnum
+{
+    Spring, Summer, Fall, Winter
+}
+#endregion
 
 public class TimeManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private int minutes;
+    public int Minutes { get { return minutes; } set { minutes = value; OnMinutesChange(value); } }
+    private int hours;
+    public int Hours { get { return hours; } set { hours = value; OnHoursChange(value); } }
+    private int days;
+    public int Days { get { return days; } set { days = value; OnDaysChange(value); } }
+
+    private float tempSeconds;
+
+    [SerializeField] private TMP_Text minute;
+    [SerializeField] private TMP_Text hour;
+
+    public DayEnum day;
+    public StationEnum yearStation;
+
+    public float roamerCalculator;
+    public void Update()
     {
-        
+        tempSeconds += Time.deltaTime;
+        if (tempSeconds >= 1)
+        {
+            Minutes += 1;
+            tempSeconds = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMinutesChange(int value)
     {
-        
+        //Debug.Log("Minutes Update");
+        if (value >= 60)
+        {
+            Hours++;
+            minutes = 0;
+        }
+        if (Hours >= 24)
+        {
+            Hours = 0;
+            Days++;
+        }
+        minute.text = minutes.ToString();
+    }
+    private void OnHoursChange(int value)
+    {
+        Debug.Log("Hour change called");
+        hour.text = hours.ToString();
+    }
+    private void OnDaysChange(int value)
+    {
+        throw new NotImplementedException();
     }
 }
